8318abc76a625eb7962321b8d0454ae1cc55dae0
Amitav Roy <reachme@amitavroy.com>
2023-09-02 13:32:46 +0530

    Video 11 - More ways to work with events

--- app/Livewire/EventPage.php
+++ app/Livewire/EventPage.php
@@ -2,10 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Events\ThirdChildComponent;
 use Livewire\Component;
 
 class EventPage extends Component
 {
+    public array $names = [
+        'Amitav', 'Jhon', 'Jane'
+    ];
+
+    public function sendToThird()
+    {
+        $this->dispatch('got-name', 'Checking')->to(ThirdChildComponent::class);
+    }
+
     public function render()
     {
         return view('livewire.event-page');
--- resources/views/livewire/event-page.blade.php
+++ resources/views/livewire/event-page.blade.php
@@ -1,5 +1,23 @@
 <div>
-    <livewire:events.main-component />
+<livewire:events.main-component />
     <hr>
     <livewire:events.independent />
+    <div x-on:got-name.window="alert('Hi')">
+        <p>A different div</p>
+    </div>
+    <hr>
+    <div>
+        <button @click="$dispatch('got-name', {name: 'Amitav Roy'})">Direct dispact</button>
+    </div>
+    <div>
+        @foreach($names as $name)
+            <button @click="$dispatch('got-name', {name: '{{$name}}'})">{{ $name }}</button>
+        @endforeach
+    </div>
+    <hr>
+    <div>
+        @foreach($names as $name)
+            <button wire:click="sendToThird">{{ $name }}</button>
+        @endforeach
+    </div>
 </div>
